516ff70ab8e64ac0195a3bb5af791f0587929b16
Aziz <aziz@Azizs-MacBook-Air.local>
2024-10-15 09:44:37 +0700

    add colors template

---
+++ lib/utils/Colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class CustomColor {
+  static const Color primary = Color(0xFFFEC800);
+
+  static const Color white = Color(0xFFFFFFFF);
+
+  static const Color black = Color(0xFF000000);
+}
