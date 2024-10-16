7405653d075e50af6f10e3a0b98283462a520806
Aziz <aziz@Azizs-MacBook-Air.local>
2024-10-16 07:48:20 +0700

    fix bug camera

---
+++ lib/pages/CameraPage.dart
@@ -0,0 +1,104 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'dart:io';
+
+class CameraPage extends StatefulWidget {
+  const CameraPage({super.key});
+
+  @override
+  State<CameraPage> createState() => _CameraPageState();
+}
+
+class _CameraPageState extends State<CameraPage> {
+  late CameraController controller;
+  bool _isCameraInitialized = false;
+  XFile? _capturedImage;
+
+  @override
+  void initState() {
+    super.initState();
+    _initializeCamera();
+  }
+
+  Future<void> _initializeCamera() async {
+    try {
+      final cameras = await availableCameras();
+      if (cameras.isNotEmpty) {
+        controller = CameraController(
+          cameras[1],
+          ResolutionPreset.max,
+        );
+
+        await controller.initialize();
+
+        if (!mounted) return;
+
+        setState(() {
+          _isCameraInitialized = true;
+        });
+      } else {
+        print('No cameras found');
+      }
+    } catch (e) {
+      print('Error initializing camera: $e');
+    }
+  }
+
+  Future<void> _takePicture() async {
+    if (!controller.value.isInitialized) {
+      print('Error: Camera is not initialized.');
+      return;
+    }
+
+    try {
+      final image = await controller.takePicture();
+      setState(() {
+        _capturedImage = image;
+      });
+    } catch (e) {
+      print('Error taking picture: $e');
+    }
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (!_isCameraInitialized) {
+      return const Center(child: CircularProgressIndicator());
+    }
+
+    return Scaffold(
+      appBar: AppBar(title: const Text('Camera Page')),
+      body: Column(
+        children: [
+          if (_capturedImage != null)
+            Expanded(
+              child: Image.file(
+                File(_capturedImage!.path),
+                fit: BoxFit.cover,
+              ),
+            )
+          else
+            Expanded(
+              child: AspectRatio(
+                aspectRatio: controller.value.aspectRatio,
+                child: CameraPreview(controller),
+              ),
+            ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _takePicture();
+        },
+        child: const Icon(Icons.camera),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
+  }
+}
