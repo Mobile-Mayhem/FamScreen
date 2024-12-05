05e5b2801f3c739e2086fc94c07a57b75d08f19f
Abdul Aziz <aziz@Abduls-MacBook-Air.local>
2024-12-05 09:25:40 +0700

    all

---
+++ lib/services/auto_picture.dart
@@ -0,0 +1,25 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+class AutoPictureService with ChangeNotifier {
+  Timer? _timer;
+
+  void startAutoPicture(Function takePicture) {
+    _timer?.cancel(); // Pastikan timer sebelumnya dihentikan
+    _timer = Timer.periodic(Duration(minutes: 5), (timer) {
+      takePicture();
+      print('Auto-take-picture running...');
+    });
+  }
+
+  void stopAutoPicture() {
+    _timer?.cancel();
+    print('Auto-take-picture stopped.');
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
+  }
+}
