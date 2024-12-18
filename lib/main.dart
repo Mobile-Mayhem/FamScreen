862dc5f724eb5aed35b9e7741392313a7c54f421
Abdul Aziz <aziz@Abduls-MacBook-Air.local>
2024-12-18 13:06:46 +0700

    all

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:famscreen/pages/CameraPage.dart';
 import 'package:famscreen/pages/HomePage.dart';
 import 'package:famscreen/pages/OnBoardingPage.dart';
 import 'package:famscreen/utils/Colors.dart';
